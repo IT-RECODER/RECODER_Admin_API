ae1b42b02b92012e2b2489091e7695e3fd6f5589
DevRappers <devrappers@gmail.com>
2020-08-10 12:21:33 +0900

    [Add] Morgan, [FIX] port number

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
-require('dotenv').config();
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
-const userRouter = require('./routes/user');
+const userRouter = require("./routes/user");
 const { sequelize } = require("./models");
 
-sequelize
-    .sync()
-    .then(() => {
-        console.log("db 연결 성공");
-    })
-    .catch(console.error);
+dotenv.config();
+const app = express();
 
+sequelize
+  .sync()
+  .then(() => {
+    console.log("db 연결 성공");
+  })
+  .catch(console.error);
+
+app.use(morgan("dev"));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/users", userRouter);
 
-app.listen(3000, () => {
-    console.log("서버 시작");
+app.listen(5000, () => {
+  console.log("실행중 http://localhost:5000/");
 });
