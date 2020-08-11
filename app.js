8d0db8e3ae5e7afc9ece2049fbc4d410153937d2
DevRappers <devrappers@gmail.com>
2020-08-11 20:51:07 +0900

    add main api

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
 
-const userRouter = require("./routes/user");
-const { sequelize } = require("./models");
+const userRouter = require('./routes/user');
+const mainRouter = require('./routes/main');
+const { sequelize } = require('./models');
 
 dotenv.config();
 const app = express();
@@ -13,17 +15,19 @@ const app = express();
 sequelize
   .sync()
   .then(() => {
-    console.log("db 연결 성공");
+    console.log('db 연결 성공');
   })
   .catch(console.error);
 
-app.use(morgan("dev"));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(morgan('dev'));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/users", userRouter);
+app.use('/users', userRouter);
+app.use('/main', mainRouter);
 
 app.listen(5000, () => {
-  console.log("실행중 http://localhost:5000/");
+  console.log('실행중 http://localhost:5000/');
 });
---
+++ routes/main.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const multer = require('multer');
+const { Main } = require('../models');
+
+const router = express.Router();
+
+try {
+  fs.accessSync('uploads');
+} catch (error) {
+  console.log('uploads 폴더가 없으므로 생성합니다.');
+  fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads');
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      const basename = path.basename(file.originalname, ext);
+      done(null, basename + '_' + new Date().getTime() + ext);
+    },
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 },
+});
+
+// main data 생성.
+router.post('/', upload.single('image'), async (req, res, next) => {
+  try {
+    const currMain = Main.findOne({ where: { id: 1 } });
+
+    if (currMain) {
+      await Main.update(
+        {
+          title: req.body.title,
+          description: req.body.description,
+          photo: req.file.filename,
+        },
+        {
+          where: { id: 1 },
+        },
+      );
+    } else {
+      await Main.create({
+        title: req.body.title,
+        description: req.body.description,
+        photo: req.file.filename,
+      });
+    }
+
+    res.status(200).json({
+      title: req.body.title,
+      description: req.body.description,
+      photo: req.file.filename,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// main data 수정.
+router.patch('/', upload.single('image'), async (req, res, next) => {
+  try {
+    await Main.update(
+      {
+        title: req.body.title,
+        description: req.body.description,
+        photo: req.body.photo,
+      },
+      {
+        where: { id: 1 },
+      },
+    );
+
+    res.status(200).json({
+      title: req.body.title,
+      description: req.body.description,
+      photo: req.file.filename,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// main 데이터 불러오기.
+router.get('/', async (req, res, next) => {
+  try {
+    const mainData = Main.findOne({ where: { id: 1 } });
+    if (!mainData) {
+      return res.status(404).send('아직 메인데이터가 없습니다.');
+    }
+    res.status(200).json(mainData);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
